78ec54bc54e7886cb3c1c4748971d6c862e6fb9d
NovikovStan <novikov.stan@mail.ru>
2019-12-25 15:37:16 +0300

    lesson5 init

--- static_src/components/ChatList.jsx
+++ static_src/components/ChatList.jsx
@@ -1,27 +1,59 @@
-import React from "react";
-import { List, ListItem } from "material-ui/List";
-import { Link } from "react-router-dom";
-import ContentSend from "material-ui/svg-icons/content/send";
-import Divider from "material-ui/Divider";
-import Subheader from "material-ui/Subheader";
-import "../styles/styles.css";
+import React from 'react';
+import {bindActionCreators} from "redux";
+import connect from "react-redux/es/connect/connect";
+import { Link } from 'react-router-dom'
+import { List, ListItem } from 'material-ui/List';
+import AddIcon from 'material-ui/svg-icons/content/add';
+import ContentSend from 'material-ui/svg-icons/content/send';
+import { addChat } from '../actions/chatActions';
 
-const ChatList = () => (
-  <div className="chart-list">
-    <List>
-      <Subheader style={{ fontSize: "22px" }}>Chat list:</Subheader>
-      <Divider />
-      <Link to="/chat/1">
-        <ListItem primaryText="Chatroom1" leftIcon={<ContentSend />} />
-      </Link>
-      <Link to="/chat/2">
-        <ListItem primaryText="Chatroom2" leftIcon={<ContentSend />} />
-      </Link>
-      <Link to="/chat/3">
-        <ListItem primaryText="Chatroom3" leftIcon={<ContentSend />} />
-      </Link>
-    </List>
-  </div>
-);
 
-export default ChatList;
+class ChatList extends React.Component {
+ 
+  // handleChange = event => {
+  //   this.setState({ [event.target.name]: event.target.value });
+  // };
+
+  // handleKeyUp = event => {
+  //   if (event.keyCode === 13) {
+  //     // Enter
+  //     this.handleAddChat();
+  //   }
+  // };
+
+  handleAddChat = () => {
+    this.props.addChat();
+  };
+
+  render() {
+    const { chats } = this.props.chats;
+    const chatElements = Object.keys(chats).map(chatId => (
+      <Link key={chatId} to={`/chat/${chatId}`}>
+        <ListItem
+          primaryText={chats[chatId].title}
+          leftIcon={<ContentSend />}
+        />
+      </Link>
+    ));
+
+    return (
+      <List>
+        {chatElements}
+        <ListItem
+          key="Add new chat"
+          leftIcon={<AddIcon />}
+          onClick={this.handleAddChat}
+          style={{ height: "60px" }}
+        />
+      </List>
+    );
+  }
+}
+
+const mapStateToProps = ({ chatReducer }) => ({
+  chats: chatReducer.chats,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators({ addChat }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(ChatList);
